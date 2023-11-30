4fa83b403848618eaec4a1c8caf946ffa88c541f
truthfuldev <truthfuldev@gmail.com>
2023-11-30 20:51:44 +0100

    fix job/bedget

--- src/app/job/budget/page.tsx
+++ src/app/job/budget/page.tsx
@@ -12,104 +12,106 @@ export const metadata: Metadata = {
 export default function Budget() {
   return (
     <>
-      <h2 className="ml-[24px] mt-[100px] text-[24px] font-[700]">
+      <h2 className="ml-[24px] mt-[100px] text-[24px] font-[700] lg:ml-[15%]">
         Budget type & Rate
       </h2>
 
-      <section className="mt-[24px] flex h-auto w-full flex-col gap-[36px] rounded-md border border-black p-4">
-        <div className="flex h-auto flex-col items-center justify-around gap-[48px] rounded-lg p-12 lg:flex-row">
-          <div className="w-[300px] rounded-md border border-black py-6 text-center text-[18px] font-[600]">
-            Hourly Rate
-          </div>
-
-          <div className="flex flex-col gap-[48px]">
-            <div className="flex flex-row items-center gap-[36px]">
-              <p className="text-[16px] font-[500]">minium:</p>
-
-              <div className="flex flex-row items-center gap-[12px]">
-                <div className="relative flex max-w-[180px]">
-                  <Input
-                    placeholder="15"
-                    className="text-center text-[14px] font-[700] "
-                  />
-
-                  <div className="absolute right-[5px] top-[4px]">
-                    <TiArrowSortedUp />
-                    <TiArrowSortedDown />
-                  </div>
-                </div>
-
-                <span className="text-[14px] font-[400]">$</span>
-              </div>
+      <section className="flex justify-center">
+        <div className="mt-[24px] flex h-auto w-full flex-col gap-[36px] rounded-md border border-black p-4 lg:w-[80%] xl:w-[70%]">
+          <div className="flex h-auto flex-col items-center justify-around gap-[48px] rounded-lg p-12 lg:flex-row">
+            <div className="w-[300px] rounded-md border border-black py-6 text-center text-[18px] font-[600]">
+              Hourly Rate
             </div>
 
-            <div className="flex flex-row items-center gap-[32px]">
-              <p className="text-[16px] font-[500]">maxium:</p>
+            <div className="flex flex-col gap-[48px]">
+              <div className="flex flex-row items-center gap-[36px]">
+                <p className="text-[16px] font-[500]">minium:</p>
 
-              <div className="flex flex-row items-center gap-[12px]">
-                <div className="relative flex max-w-[180px]">
-                  <Input
-                    placeholder="30"
-                    className="text-center text-[14px] font-[700] "
-                  />
+                <div className="flex flex-row items-center gap-[12px]">
+                  <div className="relative flex max-w-[180px]">
+                    <Input
+                      placeholder="15"
+                      className="text-center text-[14px] font-[700] "
+                    />
 
-                  <div className="absolute right-[5px] top-[4px]">
-                    <TiArrowSortedUp />
-                    <TiArrowSortedDown />
+                    <div className="absolute right-[5px] top-[4px]">
+                      <TiArrowSortedUp />
+                      <TiArrowSortedDown />
+                    </div>
                   </div>
-                </div>
 
-                <span className="text-[14px] font-[400]">$</span>
+                  <span className="text-[14px] font-[400]">$</span>
+                </div>
+              </div>
+
+              <div className="flex flex-row items-center gap-[32px]">
+                <p className="text-[16px] font-[500]">maxium:</p>
+
+                <div className="flex flex-row items-center gap-[12px]">
+                  <div className="relative flex max-w-[180px]">
+                    <Input
+                      placeholder="30"
+                      className="text-center text-[14px] font-[700] "
+                    />
+
+                    <div className="absolute right-[5px] top-[4px]">
+                      <TiArrowSortedUp />
+                      <TiArrowSortedDown />
+                    </div>
+                  </div>
+
+                  <span className="text-[14px] font-[400]">$</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="border border-gray-300"></div>
+          <div className="border border-gray-300"></div>
 
-        <div className="mb-[24px] flex h-auto flex-col items-center justify-around gap-[48px] rounded-lg p-12 lg:flex-row">
-          <div className="w-[300px] rounded-md border border-black py-6 text-center text-[18px] font-[600]">
-            Project Budget
-          </div>
-
-          <div className="flex flex-col gap-[48px]">
-            <div className="flex flex-row items-center gap-[36px]">
-              <p className="text-[16px] font-[500]">minium:</p>
-
-              <div className="flex flex-row items-center gap-[12px]">
-                <div className="relative flex max-w-[180px]">
-                  <Input
-                    placeholder="300"
-                    className="text-center text-[14px] font-[700] "
-                  />
-
-                  <div className="absolute right-[5px] top-[4px]">
-                    <TiArrowSortedUp />
-                    <TiArrowSortedDown />
-                  </div>
-                </div>
-
-                <span className="text-[14px] font-[400]">$</span>
-              </div>
+          <div className="mb-[24px] flex h-auto flex-col items-center justify-around gap-[48px] rounded-lg p-12 lg:flex-row">
+            <div className="w-[300px] rounded-md border border-black py-6 text-center text-[18px] font-[600]">
+              Project Budget
             </div>
 
-            <div className="flex flex-row items-center gap-[32px]">
-              <p className="text-[16px] font-[500]">maxium:</p>
+            <div className="flex flex-col gap-[48px]">
+              <div className="flex flex-row items-center gap-[36px]">
+                <p className="text-[16px] font-[500]">minium:</p>
 
-              <div className="flex flex-row items-center gap-[12px]">
-                <div className="relative flex max-w-[180px]">
-                  <Input
-                    placeholder="5000"
-                    className="text-center text-[14px] font-[700] "
-                  />
+                <div className="flex flex-row items-center gap-[12px]">
+                  <div className="relative flex max-w-[180px]">
+                    <Input
+                      placeholder="300"
+                      className="text-center text-[14px] font-[700] "
+                    />
 
-                  <div className="absolute right-[5px] top-[4px]">
-                    <TiArrowSortedUp />
-                    <TiArrowSortedDown />
+                    <div className="absolute right-[5px] top-[4px]">
+                      <TiArrowSortedUp />
+                      <TiArrowSortedDown />
+                    </div>
                   </div>
-                </div>
 
-                <span className="text-[14px] font-[400]">$</span>
+                  <span className="text-[14px] font-[400]">$</span>
+                </div>
+              </div>
+
+              <div className="flex flex-row items-center gap-[32px]">
+                <p className="text-[16px] font-[500]">maxium:</p>
+
+                <div className="flex flex-row items-center gap-[12px]">
+                  <div className="relative flex max-w-[180px]">
+                    <Input
+                      placeholder="5000"
+                      className="text-center text-[14px] font-[700] "
+                    />
+
+                    <div className="absolute right-[5px] top-[4px]">
+                      <TiArrowSortedUp />
+                      <TiArrowSortedDown />
+                    </div>
+                  </div>
+
+                  <span className="text-[14px] font-[400]">$</span>
+                </div>
               </div>
             </div>
           </div>
