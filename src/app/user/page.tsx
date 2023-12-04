82edef7bcdbbe8b070e51efb13025459ff4fdf05
truthfuldev <truthfuldev@gmail.com>
2023-12-04 09:14:00 +0100

    fix error in user page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -107,57 +107,5 @@ export default function User() {
         </TableBody>
       </Table>
     </section>
-    // <section className="p-4">
-    //   <div>
-    //     <Table>
-    //       <TableHeader>
-    //         <TableRow>
-    //           <TableHead className="w-[100px]">id</TableHead>
-    //           <TableHead>Full Name</TableHead>
-    //           <TableHead>Email</TableHead>
-    //           <TableHead>UserName</TableHead>
-    //           <TableHead>Role</TableHead>
-    //           <TableHead></TableHead>
-    //         </TableRow>
-    //       </TableHeader>
-    //       <TableBody>
-    //         <TableRow>
-    //           <TableCell className="font-medium">01</TableCell>
-    //           <TableCell>Ivan Kovalenko</TableCell>
-    //           <TableCell>ivan@gmail.com</TableCell>
-    //           <TableCell>Ivan</TableCell>
-    //           <TableCell>Developer</TableCell>
-
-    //           <TableCell className="flex flex-row items-center justify-center gap-[20px]">
-    //             <Link href="user/1">
-    //               <Button className="bg-transparent hover:border hover:bg-transparent">
-    //                 <HiOutlinePencilSquare className="text-lg text-black" />
-    //               </Button>
-    //             </Link>
-
-    //             <AlertDialog>
-    //               <AlertDialogTrigger>
-    //                 <Button className="bg-transparent hover:border hover:bg-transparent">
-    //                   <MdDeleteOutline className="text-xl text-black" />
-    //                 </Button>
-    //               </AlertDialogTrigger>
-    //               <AlertDialogContent>
-    //                 <AlertDialogHeader>
-    //                   <AlertDialogDescription>
-    //                     Are you sure want to delete?
-    //                   </AlertDialogDescription>
-    //                 </AlertDialogHeader>
-    //                 <AlertDialogFooter>
-    //                   <AlertDialogCancel>no</AlertDialogCancel>
-    //                   <AlertDialogAction>yes</AlertDialogAction>
-    //                 </AlertDialogFooter>
-    //               </AlertDialogContent>
-    //             </AlertDialog>
-    //           </TableCell>
-    //         </TableRow>
-    //       </TableBody>
-    //     </Table>
-    //   </div>
-    // </section>
   );
 }
